2b7e0f7fa4e7a415743612b4ceee8d612c616fda
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-01-13 18:46:20 +0000

    Update jokes.js

--- jokes.js
+++ jokes.js
@@ -1,25 +1,25 @@
 const jokeListArr = [
     {
         'question': 'I Iike my *space1* like I like my *space2*...',
-        'space1': 'maths problems,data analysis,maths textbooks,Chalkdust articles',
-        'space2': 'bananas,knickerbocker glories,wellington boots,prime ministers,reality TV shows,pets',
+        'space1': 'maths problems,data analysis,maths textbooks,Chalkdust articles,proofs,theorem names',
+        'space2': 'bananas,knickerbocker glories,wellington boots,prime ministers,reality TV shows,pets,shampoo adverts,eggs,eurovision songs,soft furnishings',
     },
     {
         'question': 'What did the *space1* say to the *space2*?',
-        'space1': 'circle,tangent,hexagon,right angle,zero,equals sign',
-        'space2': 'square,curve,parallelogram,negative number,multiplication sign,ruler',
+        'space1': 'circle,hexagon,right angle,zero,equals sign,maths teacher,graph,travelling salesperson',
+        'space2': 'square,parallelogram,negative number,multiplication sign,ruler,Roman numeral,Möbius strip',
     },
     {
         'question':  'A *space1* walks into a bar...',
-        'space1': 'mathematician,statistician,circle,straight line,protractor,calculator',
+        'space1': 'mathematician,statistician,circle,straight line,protractor,calculator,topologist,hypothesis,imaginary number,pair of parallel lines',
     },
     {
         'question': 'Why did the *space1* cross the road?',
-        'space1': 'Non-specific dynamic geometry software,calculator,female mathematician,maths communicator,maths student,maths homework,angle',
+        'space1': 'Non-specific dynamic geometry software,calculator,female mathematician,maths communicator,maths student,maths homework,angle,polygon,Greek letter',
     },
     {
         'question': 'How many *space1* does it take to change a lightbulb?',
-        'space1': 'maths teachers,parents who hate maths,maths communicators,spouses of maths communicators,Pythagorases',
+        'space1': 'maths teachers,parents who hate maths,maths communicators,spouses of maths communicators,Pythagorases,averages',
     },
 ]
 
